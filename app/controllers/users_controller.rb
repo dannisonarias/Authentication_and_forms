e8040696001f158dbaf516aeeba1cf042445cc00
DannisonArias <dannisonarias@gmail.com>
2020-04-03 13:35:36 -0400

    create edit routes and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
     def new
-        # instantiate User to use with form_for tag
         @user = User.new()
     end
 
@@ -14,7 +13,16 @@ class UsersController < ApplicationController
     end
 
     def edit
-        
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          redirect_to edit_user_path
+        else
+          render :edit
+        end
     end
 
     def user_params
