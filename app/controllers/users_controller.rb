bf9efb5c88ed8f81c411e5a075cfc1fcaf88cf62
DannisonArias <dannisonarias@gmail.com>
2020-04-03 11:59:18 -0400

    create form for new action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
     def new
+        # instantiate User to use with form_for tag
+        @user = User.new()
     end
+
     def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        
+    end
+
+    def user_params
+        params.require(:user).permit(:email,:password,:username)
     end
 end
